24fbaf3a38036be3de343488c7c9cc8cf401bd86
John Sichi <jsichi@gmail.com>
2016-09-06 18:13:42 -0700

    Improved RollWithMe icon

--- leash/icon/regen.sh
+++ leash/icon/regen.sh
@@ -2,7 +2,12 @@
 # Regenerate icon resources from leash.svg using ImageMagick
 
 rsvg-convert -w 512 -h 512 -o leash.png leash.svg
+rsvg-convert -w 512 -h 512 -o leash_unframed.png leash_unframed.svg
 rsvg-convert -w 36 -h 36 -o ../src/main/res/drawable-ldpi/icon.png leash.svg
 rsvg-convert -w 48 -h 48 -o ../src/main/res/drawable-mdpi/icon.png leash.svg
 rsvg-convert -w 72 -h 72 -o ../src/main/res/drawable-hdpi/icon.png leash.svg
 rsvg-convert -w 96 -h 96 -o ../src/main/res/drawable-xhdpi/icon.png leash.svg
+rsvg-convert -w 36 -h 36 -o ../src/main/res/drawable-ldpi/icon_unframed.png leash_unframed.svg
+rsvg-convert -w 48 -h 48 -o ../src/main/res/drawable-mdpi/icon_unframed.png leash_unframed.svg
+rsvg-convert -w 72 -h 72 -o ../src/main/res/drawable-hdpi/icon_unframed.png leash_unframed.svg
+rsvg-convert -w 96 -h 96 -o ../src/main/res/drawable-xhdpi/icon_unframed.png leash_unframed.svg
